eee8f094edcf283e9872395efcbe8f9b928db524
Peti <p.peti013@gmail.com>
2017-04-15 16:40:13 +0200

    Váltó

--- src/Class/ModellVasut/Váltó.java
+++ src/Class/ModellVasut/Váltó.java
@@ -7,16 +7,24 @@ package Class.ModellVasut;
  * @created 11-márc.-2017 3:39:56
  */
 
+import java.util.List;
+
 /**
  * A váltót megvalósító osztály
  */
 public class Váltó extends Csomópont {
 
-	private int aktív;
+	private SínElem aktív;
 	private SínElem rögzített;
 
-	public Váltó(){
-
+	/**
+	 * A konstruktorban beállítjuk a lista elsõ elemét rögzítettnek, a másodikat aktívnak.
+	 * A Váltót Csomópontként, annak konstruktorával hozzuk létre.
+	 */
+	public Váltó(int a, int b, List<SínElem> list){
+		super(a, b, list);
+		rögzített = list.get(0);
+		aktív = list.get(1);
 	}
 
 	public void finalize() throws Throwable {
@@ -24,15 +32,34 @@ public class Váltó extends Csomópont {
 	}
 
 	/**
-	 * a felhasználó váltását kezelõ függvény
+	 * a felhasználó váltását kezelõ függvény.
+	 * Az aktív végét a váltónak a sorban következõ
+	 * (vagy az utolsót követve az elsõ) SínElemre változtatja.
 	 */
 	public void felhasználóAkció(){
-		System.out.println(">>Váltó::vált()");
-		this.vált();
-
+		//egy változó segítségével lépünk az elemek között
+		int i = befutóSínek.indexOf(aktív);
+		// léptetjük a változónkat
+		i++;
+		// ha túlléptünk a listán, elõrõl kezdjük (1-tõl, mivel a 0. a rögzített)
+		if (i >= befutóSínek.size())
+			i = 1;
+		// ha léptetés után nem az aktívon állunk, akkor lecseréljük azt az aktuálisra
+		// ha léptetés után is az aktív az aktuális, akkor nem változtatunk semmit
+		if (befutóSínek.get(i) != aktív)
+			aktív = befutóSínek.get(i);
 	}
 
-	private void vált(){
-
-	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
